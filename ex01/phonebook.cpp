6eb29e8e6d73d4ed3d70146e2843d3a15ded8488
somedevv <adriancrmini@gmail.com>
2022-03-16 16:54:28 +0100

    Added main (not final)

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,59 +1,37 @@
-#include <iostream>
-#include <sstream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 11:50:20 by agaliste          #+#    #+#             */
+/*   Updated: 2022/03/16 16:54:14 by agaliste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-class PHONEBOOK {
-	public:
-		PHONEBOOK(std::string fname, std::string lname, std::string nickname, std::string darksecret, std::string phone){
-			_Fname = fname;
-			_Lname = lname;
-			_Nickname = nickname;
-			_Darksecret = darksecret;
-			_Phone = phone;
-		}
-		std::string getFName(){
-			return _Fname;
-		}
-		std::string getLName(){
-			return _Lname;
-		}
-		std::string getNickname(){
-			return _Nickname;
-		}
-		std::string getDarksecret(){
-			return _Darksecret;
-		}
-		std::string getPhone(){
-			return _Phone;
-		}
-		~PHONEBOOK(){}
-		std::string toString(){
-			std::stringstream s;
-			s << "First Name: " << _Fname << std::endl;
-			s << "last Name: " << _Lname << std::endl;
-			s << "Nickname: " << _Nickname << std::endl;
-			s << "Dark Secret: " << _Darksecret << std::endl;
-			s << "Phone: " << _Phone << std::endl;
-			return s.str();
-		}
+#include "phonebook.hpp"
 
-	private:
-		std::string _Fname;
-		std::string _Lname;
-		std::string _Nickname;
-		std::string _Darksecret;
-		std::string	_Phone;
-};
-
-int main(){
+int	main(void)
+{
+	User phonebook[9];
 	std::string command = " ";
+
 	while (command != "EXIT")
 	{
 		std::cout << "Command: ";
 		std::cin >> command;
 		if (command == "EXIT")
-			exit(0);
-		if (command == "ADD"){}
+			break;
+		if (command == "ADD"){
+			phonebook[0].setFName();
+			phonebook[0].setLName();
+			phonebook[0].setNickname();
+			phonebook[0].setDarksecret();
+			phonebook[0].setPhone();
+			phonebook[0].printUser();
+		}
 		if (command == "SEARCH"){}
 	}
-	return 0;
+	exit (0);
 }
