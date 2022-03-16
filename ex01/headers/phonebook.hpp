04aacdefa4b5dd47e8733fd15eb05cf7b2abdada
somedevv <adriancrmini@gmail.com>
2022-03-16 17:03:55 +0100

    [ex01] Updated cin variable in class

--- ex01/headers/phonebook.hpp
+++ ex01/headers/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 11:17:09 by agaliste          #+#    #+#             */
-/*   Updated: 2022/03/16 16:52:35 by agaliste         ###   ########.fr       */
+/*   Updated: 2022/03/16 17:02:40 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@
 class User { 
 	public: 
 		void setFName(){
-			std::string command;
+			std::string str;
 			while (true)
 			{
 				std::cout << "First Name: ";
-				std::cin >> command;
-				if (std::regex_match(command, std::regex("^[A-Za-z]+$"))){
-					_Fname = command;
+				std::cin >> str;
+				if (std::regex_match(str, std::regex("^[A-Za-z]+$"))){
+					_Fname = str;
 					break;
 				}
 				else {
@@ -35,13 +35,13 @@ class User {
 			}
 		}
 		void setLName(){
-			std::string command;
+			std::string str;
 			while (true)
 			{
 				std::cout << "Last Name: ";
-				std::cin >> command;
-				if (std::regex_match(command, std::regex("^[A-Za-z]+$"))){
-					_Lname = command;
+				std::cin >> str;
+				if (std::regex_match(str, std::regex("^[A-Za-z]+$"))){
+					_Lname = str;
 					break;
 				}
 				else {
@@ -50,13 +50,13 @@ class User {
 			}
 		}
 		void setNickname(){
-			std::string command;
+			std::string str;
 			while (true)
 			{
 				std::cout << "Nickname: ";
-				std::cin >> command;
-				if (std::regex_match(command, std::regex("^[A-Za-z]+$"))){
-					_Nickname = command;
+				std::cin >> str;
+				if (std::regex_match(str, std::regex("^[A-Za-z]+$"))){
+					_Nickname = str;
 					break;
 				}
 				else {
@@ -65,13 +65,13 @@ class User {
 			}
 		}
 		void setDarksecret(){
-			std::string command;
+			std::string str;
 			while (true)
 			{
 				std::cout << "Dark secret: ";
-				std::cin >> command;
-				if (std::regex_match(command, std::regex("^[A-Za-z]+$"))){
-					_Darksecret = command;
+				std::cin >> str;
+				if (std::regex_match(str, std::regex("^[A-Za-z]+$"))){
+					_Darksecret = str;
 					break;
 				}
 				else {
@@ -80,13 +80,13 @@ class User {
 			}
 		}
 		void setPhone(){
-			std::string command;
+			std::string str;
 			while (true)
 			{
 				std::cout << "Phone Number: ";
-				std::cin >> command;
-				if (std::regex_match(command, std::regex("^[0-9]+$")) && command.length() == 9) {
-					_Phone = command;
+				std::cin >> str;
+				if (std::regex_match(str, std::regex("^[0-9]+$")) && str.length() == 9) {
+					_Phone = str;
 					break;
 				}
 				else {
